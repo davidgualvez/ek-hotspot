8846c298c920f8f06747d084a13f19c5a62a2255
david <gualvezdavid@gmail.com>
2019-01-05 19:08:24 +0800

    stable version

--- js/pages/logout.js
+++ js/pages/logout.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
-	login();
+
+	$.confirm({
+		title: 'Thank you!',
+		content: 'Click Confirm button to connect again.',
+		type: 'purple',
+		typeAnimated: true,
+		boxWidth: '75%',
+		useBootstrap: false,
+		buttons: {
+			confirm: function () { 
+				login();
+			}
+		}
+	});
+
 });
 
-function login(){
-	$('#btnLogin').on('click', function(){
-		var link = $('#redirect_to_login_url').val(); 
-		redirectTo(link);
-	});
+function login(){ 
+	var link = $('#redirect_to_login_url').val(); 
+	redirectTo(link); 
  }
